6c3fc5b709bcc9b05a5b77ffe683e831daa6a8e4
Mike Ralphson <mike.ralphson@gmail.com>
2018-11-22 18:02:35 +0000

    More swagger1!

--- swagger1.js
+++ swagger1.js
@@ -7,5 +7,12 @@ const cheerio = require('cheerio');
 const str = fs.readFileSync('./swagger1.html','utf8');
 const $ = cheerio.load(str);
 $('tr').each(function(){
-  console.log($(this).find('td').text());
+  //console.log($(this).find('td').text());
+  $(this).children('td').each(function(){
+    console.log($(this).text());
+    const link = $(this).children('a').first();
+    if (link) {
+	console.log($(link).text(),$(link).attr('href'));
+    }
+  });
 });
